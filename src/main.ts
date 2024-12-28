5f1ebe20d1e9f7535a4472a6496a92809586bc52
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2024-12-29 00:51:29 +0100

    Feat: Swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import { setupSwagger } from './utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1/');
   const configService = app.get(ConfigService);
+
+  app.use(helmet());
+
+  app.use(
+    rateLimit({
+      windowMs: 1000 * 60 * 60,
+      max: 1000,
+      message:
+        '⚠️  Too many request created from this IP, please try again after an hour',
+    }),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,10 +32,16 @@ async function bootstrap() {
     }),
   );
 
+  // Setup Swagger
+  setupSwagger(app);
+
   const logger = new Logger('bootstrap');
 
   await app.listen(configService.get('PORT'), () => {
-    return logger.log(`Server running on port ${configService.get('PORT')}`);
+    return logger.log(`🚀 Server running on port ${configService.get('PORT')}`);
   });
 }
-bootstrap();
+bootstrap().catch((e) => {
+  Logger.error(`❌  Error starting server, ${e}`, '', 'Bootstrap', false);
+  throw e;
+});
--- src/utils/config/config.module.ts
+++ src/utils/config/config.module.ts
@@ -14,6 +14,12 @@ import * as Joi from 'joi';
         DATABASE_URL: Joi.string().required(),
         PORT: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
+        SWAGGER_DOC_TITLE: Joi.string().required(),
+        SWAGGER_DOC_DESCRIPTION: Joi.string().required(),
+        SWAGGER_DOC_VERSION: Joi.string().required(),
+        SWAGGER_PATH: Joi.string().required(),
+        SWAGGER_SITE_TITLE: Joi.string().required(),
+        SWAGGER_MODELS_EXPAND_DEPTH: Joi.number().required(),
         NODE_ENV: Joi.string()
           .valid('production', 'development', 'test')
           .default('development'),
